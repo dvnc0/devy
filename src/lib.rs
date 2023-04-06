f0ff6552f1489f9cd12e8b0595db36b26fa4c81d
dvnc0 <dcobb.dev@gmail.com>
2023-04-05 23:34:48 -0400

    Initial project setup

---
+++ src/lib.rs
@@ -0,0 +1,81 @@
+use core::panic;
+use std::fmt::Error;
+use clap::{Command, Arg, ArgAction, ArgMatches};
+
+// Not crazy about this, may change
+#[derive(Debug)]
+pub struct AppConfig {
+    command: String,
+    options: Vec<(String, bool)>,
+    values: Vec<(String, String)>
+}
+
+/**
+ * Builds out the main app commands
+ */
+pub fn build_new_app() -> Result<AppConfig, Error> {
+    let config = Command::new("devy")
+        .version("0.1.0")
+        .author("dvnc0")
+        .about("Some dev tools")
+        .subcommand_required(true)
+        .subcommand(Command::new("base64")
+            .arg(Arg::new("string")
+                .value_name("STRING")
+                .help("The string to encode or decode")
+                .required(true)
+            )
+            .arg(Arg::new("decode")
+                .long("decode")
+                .short('d')
+                .help("base64 decode the given value")
+                .action(ArgAction::SetTrue)
+            )
+            .arg(Arg::new("encode")
+                .long("encode")
+                .short('e')
+                .help("base64 encode the given value")
+                .action(ArgAction::SetTrue)
+            )
+        ).get_matches();
+    
+    let app_config = get_app_config(config);
+
+    dbg!(&app_config);
+    Ok(app_config)
+
+}
+
+/**
+ * Gets the request config
+ */
+fn get_app_config(config: ArgMatches) -> AppConfig {
+    let app_config = match config.subcommand() {
+        Some(("base64", sub_match)) => {
+            let decode = sub_match.get_flag("decode");
+            let encode = sub_match.get_flag("encode");
+
+            if !decode && !encode {
+                panic!("base64 requires either the -d or -e flag")
+            }
+
+            let option = if decode {
+                vec![("decode".to_string(), true)]
+            } else {
+                vec![("encode".to_string(), true)]
+            };
+
+            let base_string = sub_match.get_one::<String>("string").unwrap().to_string();
+            let value = vec![("string".to_string(), base_string)];
+
+            AppConfig {
+                command: "base64".to_string(),
+                options: option,
+                values: value
+            }
+        },
+        _ => unreachable!("No commands found")
+    };
+
+    app_config
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+use devy::{AppConfig, build_new_app};
+fn main() {
+    let app_config = build_new_app();
+}
